a5701b44b23e2edcc1e8084f79931025e6107a46
M.Awais <awais322455@gmail.com>
2022-10-05 12:01:42 -0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }) => (
+   <Component {...pageProps} />
+)
+
 
 export default MyApp
---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+
+const hre = require("hardhat");
+
+async function main() {
+
+
+  const FunToken = await hre.ethers.getContractFactory("FunToken");
+  const funToken = await FunToken.deploy();
+
+  await funToken.deployed();
+
+  console.log(
+    `funToken with 1 ETH and unlock timestamp ${unlockTime} deployed to ${funToken.address}`
+  );
+}
+
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
